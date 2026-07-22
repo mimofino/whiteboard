c0501445da98234aac1fa16f3f44a238e9ef261d
mimofino <mg0250109@gmail.com>
2026-07-23 00:08:47 +1000

    Delete AppConstants.java

--- AppConstants.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public final class AppConstants {
-    private AppConstants() {}
-
-    public static final String SERVICE_NAME = "WhiteboardService";
-    public static final int ç = 8888;
-}
